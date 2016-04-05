12142b4f576eb61945b0c7e4873130a859e868f6
Victor Ribeiro <victormech@gmail.com>
2016-04-05 00:54:07 -0300

    - relocating IndexOutOfBoundsException

---
+++ src/Exceptions/IndexOutOfBoundsException.php
@@ -0,0 +1,15 @@
+<?php
+
+    /**
+     * @class IndexOutOfBoundsException
+     * @user Victor Ribeiro <victormech@gmail.com>
+     * Created with PHP 5.6 generator
+     * PHP 5.6 generator created by Victor MECH - April 2016
+    */
+    
+    namespace LazyEight\BasicTypes\Exceptions;
+    
+    class IndexOutOfBoundsException extends \Exception
+    {
+
+    }
