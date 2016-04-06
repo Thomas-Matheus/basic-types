eb80369e383381c60e9e579ab64fffb9a77e06ba
Victor Ribeiro <victormech@gmail.com>
2016-04-06 19:43:28 -0300

    fixed issues #1 and #2  - fixed some bugs  - added more tests

--- src/Stringy.php
+++ src/Stringy.php
@@ -139,7 +139,7 @@ class Stringy
             $substring = mb_strtolower($suffix);
             $endOfStr = mb_strtolower($endOfStr);
         }
-        return (string) $substring === $endOfStr;
+        return $substring == $endOfStr;
     }
 
     /**
@@ -147,9 +147,8 @@ class Stringy
      * with the specified prefix.
      * Returns:
      * true if the character sequence represented by the argument is a prefix of the substring of
-     * this object starting at index toffset; false otherwise. The result is false if toffset
-     * is negative or greater than the length of this Stringobject; otherwise the result is
-     * the same as the result of the expression
+     * this object starting at index toOffset; false otherwise. The result is false if toOffset
+     * is negative or greater than the length of this Stringy Object
      *
      * @param string $prefix
      * @param int $toOffset
@@ -157,7 +156,11 @@ class Stringy
      */
     public function startsWith($prefix, $toOffset = 0)
     {
-        return (substr($prefix, (int) $toOffset, mb_strlen($prefix)) === $this->getValue());
+        $strToSearch = mb_substr($this->value, $toOffset, mb_strlen($prefix));
+        if (false !== mb_strpos($strToSearch, $prefix)) {
+            return true;
+        }
+        return false;
     }
 
     /**
--- tests/StringTest.php
+++ tests/StringTest.php
@@ -10,6 +10,7 @@ namespace Test\BasicTypes;
 
 
 use LazyEight\BasicTypes\Stringy;
+use LazyEight\BasicTypes\Exceptions\IndexOutOfBoundsException;
 
 class StringyTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,7 +22,17 @@ class StringyTest extends \PHPUnit_Framework_TestCase
     /**
      * @var string
      */
+    protected $stringyPrefix = 'TEST';
+
+    /**
+     * @var string
+     */
     protected $stringySuffix = 'RING';
+
+    /**
+     * @var string
+     */
+    protected $stringyLowerCase = 'test string';
 
     /**
      * @var string
@@ -49,8 +60,19 @@ class StringyTest extends \PHPUnit_Framework_TestCase
     protected $concatSuffix = ' CONCAT';
 
     /**
+     * @var string
+     */
+    protected $trimTest = ' ABC ';
+
+    /**
+     * @var string
+     */
+    protected $trimmedTest = 'ABC';
+
+    /**
      * @covers \LazyEight\BasicTypes\Stringy::__construct
      * @uses \LazyEight\BasicTypes\Stringy
+     * @return \LazyEight\BasicTypes\Stringy
      */
     public function testCanConstructedByStringyValue()
     {
@@ -229,48 +251,132 @@ class StringyTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers \LazyEight\BasicTypes\Stringy::__construct
      * @covers \LazyEight\BasicTypes\Stringy::endsWith
-     * @depends testConcatMethod
-     * @uses \LazyEight\BasicTypes\Stringy
-     * @param \LazyEight\BasicTypes\Stringy $str
-     */
-    public function testEndWithMethodCaseSesitiveOnTrue(Stringy $str)
-    {
-        $this->assertTrue($str->endsWith($this->concatSuffix));
-    }
-
-    /**
-     * @covers \LazyEight\BasicTypes\Stringy::__construct
-     * @covers \LazyEight\BasicTypes\Stringy::endsWith
-     * @depends testConcatMethod
-     * @uses \LazyEight\BasicTypes\Stringy
-     * @param \LazyEight\BasicTypes\Stringy $str
-     */
-    public function testEndWithMethodCaseSesitiveOnFalse(Stringy $str)
-    {
-        $this->assertFalse($str->endsWith($this->defaultStringy));
-    }
-
-    /**
-     * @covers \LazyEight\BasicTypes\Stringy::__construct
-     * @covers \LazyEight\BasicTypes\Stringy::endsWith
      * @depends testCanConstructedByStringyValue
      * @uses \LazyEight\BasicTypes\Stringy
      * @param \LazyEight\BasicTypes\Stringy $str
      */
-    public function testEndWithMethodOnTrue(Stringy $str)
+    public function testEndsWith(Stringy $str)
     {
+        $this->assertTrue($str->endsWith($this->stringySuffix));
+        $this->assertFalse($str->endsWith($this->ignoreCaseSuffix));
         $this->assertTrue($str->endsWith($this->ignoreCaseSuffix, false));
+        $this->assertFalse($str->endsWith($this->concatSuffix, false));
     }
 
     /**
      * @covers \LazyEight\BasicTypes\Stringy::__construct
-     * @covers \LazyEight\BasicTypes\Stringy::endsWith
+     * @covers \LazyEight\BasicTypes\Stringy::startsWith
      * @depends testCanConstructedByStringyValue
      * @uses \LazyEight\BasicTypes\Stringy
      * @param \LazyEight\BasicTypes\Stringy $str
      */
-    public function testEndWithMethodOnFalse(Stringy $str)
+    public function testStartsWith(Stringy $str)
     {
-        $this->assertFalse($str->endsWith($this->concatSuffix, false));
+        $this->assertTrue($str->startsWith($this->stringyPrefix));
+        $this->assertFalse($str->startsWith($this->stringySuffix));
+        $this->assertTrue($str->startsWith($this->stringySuffix, 7));
+        $this->assertFalse($str->startsWith($this->stringySuffix, 1));
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::charAt
+     * @depends testCanConstructedByStringyValue
+     * @uses \LazyEight\BasicTypes\Stringy
+     * @expectedException \LazyEight\BasicTypes\Exceptions\IndexOutOfBoundsException
+     * @param \LazyEight\BasicTypes\Stringy $str
+     */
+    public function testChartAt(Stringy $str)
+    {
+        $indexToSearch = 3;
+        $this->assertEquals($str->charAt($indexToSearch), mb_substr($str->getValue(), $indexToSearch, 1));
+        $this->assertNotEquals($str->charAt($indexToSearch), mb_substr($str->getValue(), 1, 1));
+
+        $this->expectException(IndexOutOfBoundsException::class);
+        $str->charAt($str->length() + 1);
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::indexOf
+     * @depends testCanConstructedByStringyValue
+     * @uses \LazyEight\BasicTypes\Stringy
+     * @param \LazyEight\BasicTypes\Stringy $str
+     */
+    public function testIndexOf(Stringy $str)
+    {
+        $offSet = 4;
+        $suffixIndexStartsValue = mb_strpos($str->getValue(), $this->stringySuffix);
+        $prefixIndexStartsValue = mb_strpos($str->getValue(), $this->stringyPrefix);
+        $suffixIndexStartsValueWithOffSet = mb_strpos($str->getValue(), $this->stringySuffix, $offSet);
+
+        $this->assertEquals($str->indexOf($this->stringySuffix), $suffixIndexStartsValue);
+        $this->assertNotEquals($str->indexOf($this->stringySuffix), $prefixIndexStartsValue);
+        $this->assertEquals($str->indexOf($this->stringySuffix, $offSet), $suffixIndexStartsValueWithOffSet);
+        $this->assertNotEquals($str->indexOf($this->stringySuffix, $offSet), $prefixIndexStartsValue);
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::isEmpty
+     * @depends testCanConstructedByStringyValue
+     * @uses \LazyEight\BasicTypes\Stringy
+     * @param \LazyEight\BasicTypes\Stringy $str
+     */
+    public function testisEmpty(Stringy $str)
+    {
+        $emptyString = new Stringy('');
+        $this->assertFalse($str->isEmpty());
+        $this->assertTrue($emptyString->isEmpty());
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::toLowerCase
+     * @depends testCanConstructedByStringyValue
+     * @uses \LazyEight\BasicTypes\Stringy
+     * @param \LazyEight\BasicTypes\Stringy $str
+     */
+    public function testToLowerCase(Stringy $str)
+    {
+        $this->assertEquals($str->toLowerCase(), $this->stringyLowerCase);
+        $this->assertNotEquals($str->toLowerCase(), $this->defaultStringy);
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::toUpperCase
+     * @uses \LazyEight\BasicTypes\Stringy
+     */
+    public function testToUpperCase()
+    {
+        $strToBeUpperCase = new Stringy($this->stringyLowerCase);
+        $this->assertEquals($strToBeUpperCase->toUpperCase(), $this->defaultStringy);
+        $this->assertNotEquals($strToBeUpperCase->toUpperCase(), $this->stringyLowerCase);
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::trim
+     * @uses \LazyEight\BasicTypes\Stringy
+     */
+    public function testTrim()
+    {
+        $strToBeTrimmed = new Stringy($this->trimTest);
+        $this->assertEquals($strToBeTrimmed->trim(), $this->trimmedTest);
+        $this->assertNotEquals($strToBeTrimmed->trim(), $this->trimTest);
+    }
+
+    /**
+     * @covers \LazyEight\BasicTypes\Stringy::__construct
+     * @covers \LazyEight\BasicTypes\Stringy::__toString
+     * @depends testCanConstructedByStringyValue
+     * @uses \LazyEight\BasicTypes\Stringy
+     * @param \LazyEight\BasicTypes\Stringy $str
+     */
+    public function testToString(Stringy $str)
+    {
+        $this->assertEquals($str->__toString(), $this->defaultStringy);
+        $this->assertNotEquals($str->__toString(), $this->errorStringy);
     }
 }
