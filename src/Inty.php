80a0060448e223c6dc29bf2335ebf09fec55928a
Victor Ribeiro <victormech@gmail.com>
2016-04-09 01:17:16 -0300

    Added Inty class that represents a Integer value

---
+++ src/Inty.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 09/04/16
+ * Time: 01:14
+ */
+
+namespace LazyEight\BasicTypes;
+
+
+use LazyEight\BasicTypes\Interfaces\ValueObjectInterface;
+
+class Inty implements ValueObjectInterface
+{
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->value = (int) $value;
+    }
+
+    /**
+     * int
+     */
+    public function getValue()
+    {
+        // TODO: Implement getValue() method.
+    }
+
+}
